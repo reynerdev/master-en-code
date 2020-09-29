5a85207e8c3d5777ed61f5598812fcb401be4749
reynerdev <rloza@pucp.pe>
2020-09-29 15:42:02 -0500

    ddlist

--- Learning/CS/linkedlist.js
+++ Learning/CS/linkedlist.js
@@ -1,5 +1,4 @@
 /* Using Recursion */
-
 class List {
   constructor(item, head) {
     this.item = item;
@@ -7,8 +6,7 @@ class List {
   }
 
   size() {
-    /* Base Case*/
-
+    /* Caso base*/
     if (this.head === null) {
       return 1;
     } else {
@@ -17,8 +15,8 @@ class List {
   }
 
   get(nth) {
-    console.log('object');
-    console.log(this.head);
+    // console.log('object');
+    // console.log(this.head);
     if (nth === 0) {
       return this.item;
     } else {
@@ -32,21 +30,65 @@ let lista2 = new List(2, lista);
 let lista3 = new List(3, lista2);
 let lista4 = new List(4, lista3);
 
-console.log(lista4.get(0));
+console.log('Imprimir la posicion');
+console.log(lista4.get(2));
+console.log('Imprimir Lista2');
+console.log(lista2);
+/* Version 2 */
 
-class SList {
-  constructor(item, node) {
-    this.item = item;
-    this.node = node;
+lt = new List(1, null);
+lt.head = new List(2, null);
+
+console.log(lt);
+
+/*Implenting of SList. HAVING A RECURSIVE NAKED INSTRUCTION IS STRANGE AND DIFFICULT TO UNDERSTAND. FOr that reason we need to make it simple*/
+/*SSL SLL means single limked list */
+class SLList {
+  // constructor(item, node) {
+  //   this.item = item;
+  //   this.node = node;
+  // }
+
+  constructor(x) {
+    this.first = new List(x, null);
   }
 
-  add(item) {
-    this.node = new List(item, null);
+  addLast(item) {
+    var pointer = this.first;
+
+    while (pointer.head) {
+      console.log('inside while');
+      console.log(pointer);
+      pointer = pointer.head;
+      console.log(pointer);
+    }
+
+    pointer.head = new List(item, null);
+  }
+
+  addFirst(item) {
+    this.first = new List(item, this.first);
+  }
+
+  size(){
+
+    if(this.first.head === null){
+      return 0 
+    } else 
+    {
+      return 1 + 
+    }
+
   }
 }
 
-let slist = new SList(1, null);
-slist.add(1);
-slist.add(2);
+let list = new SLList(1);
+list.addLast(2);
+list.addLast(3);
+list.addLast(4);
+console.log(list);
 
-console.log(slist);
+let listAddFirst = new SLList(1);
+listAddFirst.addFirst(2);
+listAddFirst.addFirst(3);
+console.log(listAddFirst);
